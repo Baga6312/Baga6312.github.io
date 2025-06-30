50c75d98d45b69ce375dda0e11b43b2a8f547aac
Oussema Ben Ayech <88184963+Baga6312@users.noreply.github.com>
2025-06-30 10:05:11 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   output: 'export', // Enables static export
   basePath: process.env.NODE_ENV === 'production' ? '/baga6312.github.io' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/baga6312.github.io/' : '',
+  images: {
+    unoptimized: true, // Required for static export
+  },
+    trailingSlash: true,
 }
 
 const isProd = process.env.NODE_ENV === 'production';
@@ -10,8 +14,5 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   output: 'export',
   basePath: isProd ? '' : '', // Empty for GitHub Pages root
-  assetPrefix: isProd ? '' : '', // Empty for GitHub Pages root
-  images: {
-    unoptimized: true, // Required for static export
-  },
+  assetPrefix: isProd ? '' : ''
 }
