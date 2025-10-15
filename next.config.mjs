afffdc4ca50dbaea3ebae2898a45000a10e75ebc
Baga6312 <oussema.benayech@gmail.com>
2025-10-15 23:41:38 +0100

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 export default {
   output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     unoptimized: true,
   },
-};
+;
