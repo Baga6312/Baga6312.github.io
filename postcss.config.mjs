206cd5bed6ce35c6a7d8f363684186ce9a6bfcb1
Oussema Ben Ayech <88184963+Baga6312@users.noreply.github.com>
2025-10-15 23:44:22 +0100

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,15 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let assetPrefix = ''
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
-export default config;
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+export default {
+  output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
